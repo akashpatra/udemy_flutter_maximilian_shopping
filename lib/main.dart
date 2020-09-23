5fac057531b3ea9d88321abdb5456eb7101590fe
Akash Patra <patraakash9@gmail.com>
2020-09-23 20:52:14 +0530

    feat: Providing an Orders Object

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './screens/product_overview_screen.dart';
 import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           builder: (ctx) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          builder: (ctx) => Orders(),
         ),
       ],
       child: MaterialApp(
