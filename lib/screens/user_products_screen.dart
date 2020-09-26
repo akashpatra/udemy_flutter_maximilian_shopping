07f0427cbb7d4866ed8ef040cd910a9534197050
Akash Patra <patraakash9@gmail.com>
2020-09-26 20:10:52 +0530

    feat: Using Forms & Working with Form Inputs

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../screens/edit_product_screen.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
 
@@ -15,7 +16,13 @@ class UserProductsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
-        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              })
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
